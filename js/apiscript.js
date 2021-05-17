63c21875786e3f016278b82755e5d96ed89bbb42
GianfrancoJara <gia.jara@alumnos.duoc.cl>
2021-05-17 14:01:19 -0400

    Añadido el evento mouseover

--- js/apiscript.js
+++ js/apiscript.js
@@ -1,9 +1,10 @@
 $(document).ready(function()
 {
-
-    $.get("http://api.openweathermap.org/data/2.5/weather?q=santiago&appid=12af6ee760b0e5db6ed752a12c4c7fb4",
+    $("#infoClima").mouseover(function(){
+        $.get("http://api.openweathermap.org/data/2.5/weather?q=santiago&appid=12af6ee760b0e5db6ed752a12c4c7fb4",
         function(data){
             $('#infoClima').html( 
                 "La temperatura en Santiago actualmente es de "+(Math.round(data.main.temp - 273))+" grados Celsius, con una humedad del "+data.main.humidity+" porciento." );
         });
+    });
 })
